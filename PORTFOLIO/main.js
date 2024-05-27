0c6363588f1c137b8664e0de375c95920e50512f
Chandrkant07 <147088311+Chandrkant07@users.noreply.github.com>
2024-05-28 00:28:27 +0530

    Add files via upload

---
+++ PORTFOLIO/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer","Competitive Programmer","Python Developer","Cloud Enthusiast"],
+    typeSpeed:200,
+    backSpeed:200,
+    backDelay:500, 
+    loop:true
+});
